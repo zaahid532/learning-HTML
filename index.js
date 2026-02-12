14fc7eefe6510480e9d20e55d605e4f31e42bdde
zaahid532 <zaahidazkalobma@gmail.com>
2026-02-12 19:34:13 +0700

    BELAJAR HTML

---
+++ index.js
@@ -0,0 +1,34 @@
+alert("this is my first website");
+prompt("Enter your full name");
+var name = "zaahid";
+name = 'zaahid';
+    console.log(name);
+function sayhello(){
+    return "Hello," + name;
+}
+// let color = 'blue';
+// color = 'red';
+// let color = 'orange';
+for(let i = 0; i < 5; i++){
+    console.log(i);
+
+}
+// console.log(i);
+// const pi = 3.14;
+// pi = 3.17;
+// const pi = 22/7;
+if(true){
+    const number = 18;
+}
+// console.log(number);
+const person = {name: 'zaahid', age:'13'};
+person.name = 'zaahid';
+console.log(person);
+let add = 15 + 20;
+console.log(add);
+let subsrtact = 20 - 15;
+console.log(subsrtact)
+let muliply = 20 * 20;
+console.log(multiply)
+let divide = 100 / 20;
+console.log(divide);
